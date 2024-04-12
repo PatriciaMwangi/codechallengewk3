bddb67ab4b0542cb42f37dfe77a86b6d2de20aa7
patricia <patricia.mwangi@student.moringaschool.com>
2024-04-12 09:11:42 +0300

    my first commit

--- src/index.js
+++ src/index.js
@@ -1,19 +1,40 @@
 
-// Your code here    
-    /*let ul=document.getElementById('films')
+// Your code here  
+let ul=document.getElementById('films')
     let title=document.getElementById('title')
    let ticketNum=document.getElementById('ticket-num')
     let runtime=document.getElementById('runtime')
 let description=document.getElementById('film-info')
 let showtime=document.getElementById('showtime')
 let poster=document.querySelector('img')
+
     document.addEventListener('DOMContentLoaded',function(){
         fetch  ('http://localhost:3000/films')
         .then(response=>response.json())
+        console.log(response)
         .then(data=>{
         data.forEach(episode=>{
 let li=document.createElement('li');
-li.innerText= episode.title;
+//let buttons=document.createElement('button')
+let noOfButtons=15;
+for(let i=0;i<noOfButtons;i++){
+    let buttons=document.createElement('button')
+    buttons.innerText='x'
+buttons.style.float="right"
+console.log(buttons)
+buttons.addEventListener("click",function(){
+    function deleted(e){
+        console.log(e.target)
+    }
+});
+ul.appendChild(buttons)
+}
+
+
+console.log(buttons)
+
+
+li.innerText= episode.title;//buttons;
 //li.addEventListener('click',firstMovie(episode)) cannot do this as it will invoke the function immediately.
 //and we need our function invoked afer click(line 69)
 ul.appendChild(li)
@@ -36,44 +57,27 @@ poster.src=episode.poster//to change an image we schange its source
 
     console.log(firstMovie)
      
-    }*/
+    }
     let btn=document.getElementById('buy-ticket')
 btn.addEventListener('click',
     buyTickets
 )
 
-  /*  function buyTicket(){
- fetch('http://localhost:3000/films',{
-method:'PATCH',
-headers:{
-    'Content-Type':'application/json'
-},
-body:JSON.stringify({tickets_sold:tickets_sold-1})
- }).then(res=>res.json())
-        .then(da=>console.log(da))}*/
 
 function buyTickets(){
     fetch('http://localhost:3000/films')
     .then(res=>res.json())
     .then(data=>{
-        let ticketsSold=data.tickets_sold;
-        ticketsSold--;
-       
-    fetch('http://localhost:3000/films',{
-        
-        method:'PATCH',
-        headers: {
-'Content-Type':'application/json'
-        },
-        body: JSON.stringify({tickets_sold:ticketsSold})
-    }).then(ress=>console.log(ress))
-   .then(data=>{
-      if(data.tickets_sold>0){
+      let ticketsSold= parseInt(ticketNum.innerText-1)
+      if(ticketsSold>=0)
+      {
+       ticketNum.innerText=ticketsSold
         console.log(ticketsSold)
-        
-      }
-    });
-})
-return buyTickets()
+
+}else 
+{
+
+    btn.innerText='soldout'
 }
-console.log(buyTickets)
+})}
+    
